38b8796e2d998b1fea64504324a8d0c6a498153e
Sean Igo <samwibatt@gmail.com>
2019-11-24 15:24:12 -0800

    sync

--- sram.v
+++ sram.v
@@ -3,11 +3,11 @@
 
 `default_nettype none
 
-// FOR STARTERS JUST USING CLIFFORD WOLF'S BLINKY
+// FOR STARTERS JUST USING DAN GISSELQUIST'S BLINKY
 module sram_1Mx8(
     input wire i_clk,
     output wire o_led       //temp debug
-    //here will go all the address and data lines and stuff - make the addr width variable 
+    //here will go all the address and data lines and stuff - make the addr width variable
     );
 
     parameter CBITS = 26;
